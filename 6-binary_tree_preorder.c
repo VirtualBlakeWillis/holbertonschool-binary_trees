9da7c31328795125437276770a43abd79390cec9
virtualblakewillis <bewillis101@gmail.com>
2023-03-21 13:55:44 -0600

    betty documentation

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *
+ * binary_tree_preorder - goes through a binary tree using pre-order traversal
+ * @tree: tree to preform operation on
+ * @func: function to call on each node
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
