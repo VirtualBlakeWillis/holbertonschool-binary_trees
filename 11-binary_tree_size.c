38bd45e980995e298548b1163d070a318c22cb00
virtualblakewillis <bewillis101@gmail.com>
2023-03-21 14:26:14 -0600

    t11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_size -  measures the size of a binary tree
+* @tree: tree to measure
+* Return: size of tree, or 0
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    int size = 1;
+
+    if (!tree)
+        return (0);
+    
+    if (tree->left)
+        size += binary_tree_size(tree->left);
+    
+    if (tree->right)
+        size += binary_tree_size(tree->right);
+
+    return (size);
+}
