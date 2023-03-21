0200bc6b617ec18911495f1c164db9622be576d9
virtualblakewillis <bewillis101@gmail.com>
2023-03-21 14:44:45 -0600

    betty documentation

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,11 +1,14 @@
 #include "binary_trees.h"
 
 /**
- *
+ * binary_tree_nodes - count # of nodes in a binary tree
+ * @tree: tree 2 count
+ * Return: # of nodes, or 0
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t c = 0;
+
 	if (!tree)
 		return (0);
 	if (tree->left)
