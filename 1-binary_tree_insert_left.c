2bece9987b8e80619fb09f6a146ff1150f0461a7
virtualblakewillis <bewillis101@gmail.com>
2023-03-21 12:55:27 -0600

    t1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+#include "0-binary_tree_node.c"
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ *
+ * @parent: parent of new node
+ * @value: value of new node
+ * 
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	if (!parent)
+		return (NULL);
+
+	node = binary_tree_node(parent, value);
+	if (!node)
+		return (NULL);
+	
+	if (parent->left)
+		node->left = parent->left;
+
+	parent->left = node;
+	return (node);
+}
