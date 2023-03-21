7b01bc70507852979f9d12f70dac4435786602b2
virtualblakewillis <bewillis101@gmail.com>
2023-03-21 14:41:43 -0600

    t13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ *
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t c = 0;
+	if (!tree)
+		return (0);
+	if (tree->left)
+		c += 1 + binary_tree_nodes(tree->left);
+	if (tree->right)
+		if (!tree->left)
+			c++;
+		c += binary_tree_nodes(tree->right);
+	return (c);
+}
