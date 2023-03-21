7256ae0da0216ad02e148d36fc7d0aed967b6fb4
virtualblakewillis <bewillis101@gmail.com>
2023-03-21 14:15:54 -0600

    tabs > spaces

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,13 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t leftHeight = 0, rightHeight = 0;
-    if (!tree)
-        return (0);
-    if (tree->left)
-        leftHeight = 1 + binary_tree_height(tree->left);
-    if (tree->right)
-    rightHeight = 1 + binary_tree_height(tree->right);
+	size_t leftHeight = 0, rightHeight = 0;
+	if (!tree)
+		return (0);
+	if (tree->left)
+		leftHeight = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+	rightHeight = 1 + binary_tree_height(tree->right);
 
-    return (MAX(leftHeight, rightHeight));
+	return (MAX(leftHeight, rightHeight));
 }
