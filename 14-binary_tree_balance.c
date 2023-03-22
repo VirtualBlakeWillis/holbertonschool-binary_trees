c85e1c18624a96f3a4d09b030edb9c39de615750
virtualblakewillis <bewillis101@gmail.com>
2023-03-22 13:06:52 -0600

    minor shot in dark

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-
+int binary_tree_balance_directional(const binary_tree_t *tree, int direction);
 /**
  * binary_tree_balance -  measures the balance factor of a binary tree
  * @tree: tree to measure
@@ -12,9 +12,23 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left)
-		balance += 1 + binary_tree_balance(tree->left);
+		balance += 1 + binary_tree_balance_directional(tree->left, 1);
 	if (tree->right)
-		balance -= 1 + binary_tree_balance(tree->right);
+		balance -= 1 + binary_tree_balance_directional(tree->right, -1);
 
 	return (balance);
+}
+
+int binary_tree_balance_directional(const binary_tree_t *tree, int direction)
+{
+	if (!tree)
+		return (0);
+	int balance = 0;
+	if (direction < 0)
+	{
+		return balance += 1 + binary_tree_balance_directional(tree->left, direction);
+	}
+	if (direction > 0)
+		return balance -= 1 + binary_tree_balance_directional(tree->right, direction);
+	return (0);
 }
