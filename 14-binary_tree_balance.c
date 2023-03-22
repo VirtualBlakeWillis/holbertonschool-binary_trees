b14abb7552263a46b24073696fc87178f558191b
virtualblakewillis <bewillis101@gmail.com>
2023-03-22 13:26:30 -0600

    closing scope

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -29,6 +29,7 @@ int binary_tree_balance_directional(const binary_tree_t *tree, int direction)
 		balance += direction;
 
 		return (balance += MAX(binary_tree_balance_directional(tree->left, direction),
-								binary_tree_balance_directional(tree->right, direction)))
+								binary_tree_balance_directional(tree->right, direction)));
 	}
+	return (0);
 }
