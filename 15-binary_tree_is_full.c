76816a03fe20fbcf313a0021c4501a517a0ace36
virtualblakewillis <bewillis101@gmail.com>
2023-03-22 13:38:01 -0600

    testing if old funcs work

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
-
+#include "14-binary_tree_balance.c"
 /**
 * binary_tree_is full - checks if a binary tree is full
+* @tree: tree to check
+* Return: 1 if full, 0 if not
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
