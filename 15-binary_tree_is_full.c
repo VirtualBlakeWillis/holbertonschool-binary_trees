7bf84ef87dad4791422fb4f41f24cb3b2fa364ac
virtualblakewillis <bewillis101@gmail.com>
2023-03-22 14:02:25 -0600

    ;

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,7 +13,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 
 	balance = ((1 * binary_tree_is_full(tree->left)) + 
-				(-1 * binary_tree_is_full(tree->right)))
+				(-1 * binary_tree_is_full(tree->right)));
 
 	if (balance == 0)
 		return (1);
