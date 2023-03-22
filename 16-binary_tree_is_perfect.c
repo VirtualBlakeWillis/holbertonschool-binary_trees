8df9dad575a8701c0259f6ed317a35e4acf1a93d
virtualblakewillis <bewillis101@gmail.com>
2023-03-22 14:11:08 -0600

    t16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+#include "14-binary_tree_balance.c"
+#include "15-binary_tree_full.c"
+/**
+* binary_tree_is_perfect - checks if a binary tree is perfect
+* @tree: tree to check
+* Return: 1 if perfect, 0 if not
+*/
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    int balance = 0, full = 0, perfect = 0;
+    if (!tree)
+        return (0);
+
+    full = binary_tree_full(tree);
+    balance = binary_tree_balance(tree);
+    
+    if (balance == 0 && full == 1)
+        return (1);
+    return (0);
+}
