7f77cc8b216c7c9d69b95454cc549066e901716c
virtualblakewillis <bewillis101@gmail.com>
2023-03-22 14:26:41 -0600

    betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,10 +8,10 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (!node || !node->parent)
-        return (NULL);
-    
-    return (binary_tree_sibling(node->parent));
+	if (!node || !node->parent)
+		return (NULL);
+	
+	return (binary_tree_sibling(node->parent));
 }
 
 /**
