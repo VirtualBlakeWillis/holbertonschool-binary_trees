ca428c1ec32bb8f2ffb9feebe1c33bad67ec1f01
virtualblakewillis <bewillis101@gmail.com>
2023-03-22 14:17:30 -0600

    betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -65,14 +65,14 @@ int binary_tree_is_full(const binary_tree_t *tree)
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int balance = 0, full = 0;
-    if (!tree)
-        return (0);
+	int balance = 0, full = 0;
+	if (!tree)
+		return (0);
 
-    full = binary_tree_is_full(tree);
-    balance = binary_tree_balance(tree);
-    
-    if (balance == 0 && full == 1)
-        return (1);
-    return (0);
+	full = binary_tree_is_full(tree);
+	balance = binary_tree_balance(tree);
+
+	if (balance == 0 && full == 1)
+		return (1);
+	return (0);
 }
