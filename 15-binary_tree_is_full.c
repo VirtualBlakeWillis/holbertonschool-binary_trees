9ca33e81af60647dbfddef8941904b862d9e43c0
virtualblakewillis <bewillis101@gmail.com>
2023-03-22 13:41:57 -0600

    idk man

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,23 +1,23 @@
 #include "binary_trees.h"
 #include "14-binary_tree_balance.c"
 /**
-* binary_tree_is full - checks if a binary tree is full
+* binary_tree_is_full - checks if a binary tree is full
 * @tree: tree to check
 * Return: 1 if full, 0 if not
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    int balance = 0;
+	int balance = 0;
 
-    if (!tree)
-        return (0);
-    
-    if (tree->left)
-        balance += 1 + binary_tree_is_full(tree->left);
-    if (tree->right)
-        balance += -1 + binary_tree_is_full(tree->right);
+	if (!tree)
+		return (0);
+	
+	if (tree->left)
+		balance += 1 + binary_tree_is_full(tree->left);
+	if (tree->right)
+		balance += -1 + binary_tree_is_full(tree->right);
 
-    if (balance == 0)
-        return (1);
-    return (0);
+	if (balance != 0)
+		return (1);
+	return (0);
 }
