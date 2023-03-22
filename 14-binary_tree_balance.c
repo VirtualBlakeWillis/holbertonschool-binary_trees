47d5453f01c59c3af365755594b5779f817d5a70
virtualblakewillis <bewillis101@gmail.com>
2023-03-22 13:25:57 -0600

    messing around with MAX

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+int binary_tree_balance_directional(const binary_tree_t *tree, int direction);
 /**
  * binary_tree_balance -  measures the balance factor of a binary tree
  * @tree: tree to measure
@@ -11,9 +12,23 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left)
-		balance += 1 + binary_tree_balance(tree->left);
+		balance += 1 + binary_tree_balance_directional(tree->left, 1);
 	if (tree->right)
-		balance -= 1 + binary_tree_balance(tree->right);
+		balance -= 1 + binary_tree_balance_directional(tree->right, 1);
 
 	return (balance);
+}
+
+int binary_tree_balance_directional(const binary_tree_t *tree, int direction)
+{
+	int balance = 0;
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+	{
+		balance += direction;
+
+		return (balance += MAX(binary_tree_balance_directional(tree->left, direction),
+								binary_tree_balance_directional(tree->right, direction)))
+	}
 }
