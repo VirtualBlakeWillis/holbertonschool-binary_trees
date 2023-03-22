720307b2f9e0b3cf945150dc2db8ce09c26b658c
virtualblakewillis <bewillis101@gmail.com>
2023-03-22 14:12:42 -0600

    removed file inclusion

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
-#include "14-binary_tree_balance.c"
-#include "15-binary_tree_is_full.c"
+
 /**
 * binary_tree_is_perfect - checks if a binary tree is perfect
 * @tree: tree to check
