e5897de885c431df0c06b1e32f2af6026d9446bb
virtualblakewillis <bewillis101@gmail.com>
2023-04-20 18:28:17 -0600

    duplicate values is fail, and betty tabs

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -4,13 +4,13 @@ int check_tree(const binary_tree_t *node, int value, int smallerOrLarger);
  * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
  *
  * @tree: pointer to the root node of the tree to check
-    * Return: 1 if tree is a valid BST, and 0 otherwise
+	* Return: 1 if tree is a valid BST, and 0 otherwise
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    return (check_tree(tree->left, tree->n, 0) && check_tree(tree->right, tree->n, 1));
+	if (tree == NULL)
+		return (0);
+	return (check_tree(tree->left, tree->n, 0) && check_tree(tree->right, tree->n, 1));
 }
 
 /**
@@ -22,21 +22,21 @@ int binary_tree_is_bst(const binary_tree_t *tree)
  * Return: 1 if tree is a valid BST, and 0 otherwise
  */
 int check_tree(const binary_tree_t *node, int value, int smallerOrLarger)
-    {
+	{
 
-    if (node == NULL)
-        return (1);
-    if (smallerOrLarger == 0)
-    {
-        if (node->n > value)
-            return (0);
-    }
-    else
-    {
-        if (node->n < value)
-            return (0);
-    }
+	if (node == NULL)
+		return (1);
+	if (smallerOrLarger == 0)
+	{
+		if (node->n >= value)
+			return (0);
+	}
+	else
+	{
+		if (node->n <= value)
+			return (0);
+	}
 
-    return (check_tree(node->left, value, smallerOrLarger) &&
-            check_tree(node->right, value, smallerOrLarger));
+	return (check_tree(node->left, value, smallerOrLarger) &&
+			check_tree(node->right, value, smallerOrLarger));
 }
