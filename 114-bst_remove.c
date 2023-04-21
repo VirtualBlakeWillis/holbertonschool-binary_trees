f45000290a4336d2df5aa96306c4a55b15cac4da
virtualblakewillis <bewillis101@gmail.com>
2023-04-20 18:53:36 -0600

    added search fcn to file

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -75,3 +75,21 @@ bst_t *bst_remove(bst_t *root, int value)
 	bst_remove(node->right, node->n);
 	return (root);
 }
+
+/**
+ * bst_search - searches for a value in a Binary Search Tree
+ * @tree: pointer to the root node of the tree
+ * @value: value to search for
+ * Return: pointer to the node containing a value equals to value, or NULL
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree == NULL)
+		return (NULL);
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+	else if (value > tree->n)
+		return (bst_search(tree->right, value));
+	else
+		return ((bst_t *)tree);
+}
